9f48c4786b1f809d6362f710a1ff3ad917d20250
Roger Hideki Fujii Contato <roger123cont@gmail.com>
2024-11-05 17:41:24 -0400

    final-2

--- moeda_client.py
+++ moeda_client.py
@@ -4,29 +4,41 @@ import moeda_pb2
 import moeda_pb2_grpc
 
 
+# Função principal que coleta as entradas do usuário e solicita a conversão ao servidor
 def run():
+    # Exibe ao usuário as moedas válidas
     print("\nMoedas válidas:" +
           "\nAUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, EUR," +
           "\nGBP, HKD, HRK, HUF, IDR, ILS, INR, ISK, JPY," +
           "\nKRW, MXN, MYR, NOK, NZD, PHP, PLN, RON, RUB," +
           "\nSEK, SGD, THB, TRY, USD, ZAR")
 
+    # Solicita ao usuário a moeda de origem e o valor de origem
     moeda_origem = input("\nDigite a moeda de origem: ").upper()
     try:
         valor_origem = float(input("Digite o valor de origem: "))
     except ValueError:
+        # Trata erro caso o valor de origem não seja um número válido
         print("\nErro: O valor de origem deve ser um número válido.")
         return
+    
+    # Solicita ao usuário a moeda de destino
     moeda_destino = input("Digite a moeda de destino: ").upper()
 
     try:
+        # Abre uma conexão gRPC com o servidor
         with grpc.insecure_channel('localhost:50051') as channel:
             stub = moeda_pb2_grpc.MoedaStub(channel)
             try:
+                # Realiza a solicitação de conversão ao servidor
                 reply = stub.converter(moeda_pb2.ConverterRequest(
                     moeda_origem=moeda_origem, valor_origem=valor_origem, moeda_destino=moeda_destino))
+                
+                # Exibe o resultado da conversão
                 print(
                     f'\n{moeda_origem} {reply.valor_origem} = {moeda_destino} {reply.valor_destino}')
+
+            # Tratamento de possíveis erros ao chamar o servidor
             except grpc.RpcError as e:
                 if e.code() == grpc.StatusCode.INVALID_ARGUMENT:
                     print(f"\nErro de argumento inválido: {e.details()}")
@@ -34,6 +46,8 @@ def run():
                     print(f"\nErro interno do servidor: {e.details()}")
                 else:
                     print(f"\nErro inesperado: {e.code()} - {e.details()}")
+
+    # Tratamento de erro em caso de falha de conexão com o servidor
     except grpc.RpcError as e:
         print("\nErro de conexão com o servidor.")
         logging.error(f"Erro ao tentar conectar com o servidor: {e}")
--- moeda_server.py
+++ moeda_server.py
@@ -6,21 +6,26 @@ import grpc
 import moeda_pb2
 import moeda_pb2_grpc
 
+# Chave de API para acessar o serviço de câmbio
 API_KEY = 'fca_live_PejqVyXV4C9giEKy2roO9m4uTpkiB1IUgnumO3js'
 
 
 class Moeda(moeda_pb2_grpc.MoedaServicer):
+    # Método para converter o valor de uma moeda de origem para uma moeda de destino
     def converter(self, request, context):
         try:
+            # Obtém os parâmetros do cliente
             moeda_origem = request.moeda_origem
             valor_origem = request.valor_origem
             moeda_destino = request.moeda_destino
 
+            # Lista de moedas aceitas pela aplicação
             moedas_validas = ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR",
                               "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "ISK", "JPY",
                               "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB",
                               "SEK", "SGD", "THB", "TRY", "USD", "ZAR"]
 
+            # Verifica se as moedas de origem e destino são válidas
             if moeda_origem not in moedas_validas:
                 context.set_details("Moeda de origem inválida.")
                 context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
@@ -31,34 +36,45 @@ class Moeda(moeda_pb2_grpc.MoedaServicer):
                 context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
                 return moeda_pb2.ConverterReply()
 
+            # Cria um cliente para acessar a API de câmbio
             client = freecurrencyapi.Client(API_KEY)
             try:
+                # Obtém as moedas e as taxas de câmbio da API
                 moedas = client.currencies(
                     currencies=[moeda_origem, moeda_destino])['data']
                 taxas_cambio = client.latest()['data']
             except Exception as e:
+                # Tratamento de erro caso ocorra falha ao conectar com a API
                 context.set_details("Erro ao conectar com a API de câmbio.")
                 context.set_code(grpc.StatusCode.INTERNAL)
                 return moeda_pb2.ConverterReply()
 
+            # Trunca o valor de origem para o número adequado de casas decimais
             valor_origem = self.truncar_float(
                 valor_origem, moedas[moeda_origem]['decimal_digits'])
+            
+            # Realiza a conversão de moedas usando as taxas de câmbio
             valor_destino = (
                 valor_origem / taxas_cambio[moeda_origem] * taxas_cambio[moeda_destino])
+            
+            # Ajusta o valor de destino para o número correto de casas decimais
             valor_destino = self.truncar_float(
                 valor_destino, moedas[moeda_destino]['decimal_digits'])
 
+            # Retorna a resposta com os valores convertidos
             return moeda_pb2.ConverterReply(valor_origem=valor_origem, valor_destino=valor_destino)
         except Exception as e:
+            # Tratamento de erros inesperados durante a conversão
             logging.error(f"Erro inesperado: {e}")
             context.set_details("Erro inesperado durante a conversão.")
             context.set_code(grpc.StatusCode.INTERNAL)
             return moeda_pb2.ConverterReply()
 
+    # Método auxiliar para truncar um float com um número de casas decimais especificado
     def truncar_float(self, valor, escala):
         return math.trunc(valor * (10 ** escala)) / (10 ** escala)
 
-
+# Configura e inicia o servidor gRPC
 def serve():
     porta = "50051"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
